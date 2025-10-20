0fd2df7f7f1580ce10eae30c862aa4fe0f0dd18b
PetrKornev <kornevpetya@gmail.com>
2025-10-20 18:40:02 +0300

    Ready for deploy API

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router';
-
 import MainPage from './pages/MainPage';
 
 createRoot(document.getElementById('root')).render(
